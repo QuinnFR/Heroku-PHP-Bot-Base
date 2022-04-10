068444f2876446b7d3580f8fd169708533a74a7a
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-04-10 22:56:07 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,8 +16,83 @@ $Channel_ID = "Channel_ID";
 $Channel_ID = getenv('Channel_ID');
 }
 
-ob_start();
 
+
+function telegramExecCurlRequest($handle)
+{
+    $response = curl_exec($handle);
+
+    if ($response === false) {
+        $errno = curl_errno($handle);
+        $error = curl_error($handle);
+        error_log("Curl returned error $errno: $error\n");
+        curl_close($handle);
+
+        return false;
+    }
+
+    $http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE));
+    curl_close($handle);
+
+    if ($http_code >= 500) {
+        // do not wat to DDOS server if something goes wrong
+file_get_contents("https://api.telegram.org/bot$token/setWebhook?url=https://black-widow-robot.herokuapp.com/bot.php&drop_pending_updates=true");
+        sleep(10);
+
+        return false;
+    } else if ($http_code != 200) {
+        $response = json_decode($response, true);
+        error_log("Request has failed with error {$response['error_code']}: {$response['description']}\n");
+        if ($http_code == 401) {
+            throw new Exception('Invalid access token provided');
+        }
+
+        return false;
+    } else {
+        $response = json_decode($response, true);
+        if (isset($response['description'])) {
+            error_log("Request was successfull: {$response['description']}\n");
+        }
+        $response = $response['result'];
+    }
+
+    return $response;
+}
+
+function telegramApiRequest($method, $parameters)
+{
+    if (!is_string($method)) {
+        error_log("Method name must be a string\n");
+
+        return false;
+    }
+
+    if (!$parameters) {
+        $parameters = array();
+    } else if (!is_array($parameters)) {
+        error_log("Parameters must be an array\n");
+
+        return false;
+    }
+
+    foreach ($parameters as $key => &$val) {
+        // encoding to JSON array parameters, for example reply_markup
+        if (!is_numeric($val) && !is_string($val)) {
+            $val = json_encode($val);
+        }
+    }
+    $url = API_URL . $method . '?' . http_build_query($parameters);
+    $handle = curl_init($url);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+
+    return telegramExecCurlRequest($handle);
+}
+
+
+
+ob_start();
 
 include 'class/Telegram.class.php';
 include 'iTelegram.php';
