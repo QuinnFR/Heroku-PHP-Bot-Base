ebb44c9252ba736c5e36b1870bd57968cc724335
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-04-10 16:09:39 +0300

    Update Telegram.class.php

--- class/Telegram.class.php
+++ class/Telegram.class.php
@@ -8,7 +8,7 @@ class Telegram {
         $this->token = $token;
     }
 
-    private function bot1($method , $data){
+    private function bot($method , $data){
         $url = 'https://api.telegram.org/bot'.$this->token.'/'.$method;
         $ch = curl_init($url);
 
@@ -22,97 +22,6 @@ class Telegram {
 
 
 
-private function bot($method, $data) {
-	if (!is_string($method)) {
-		error_log("Nome do método deve ser uma string\n"); 
-		return false; 
-	} 
-	if (!$data) {
-		$data = array(); 
-	} else if (!is_array($data)) {
-		error_log("Os parâmetros devem ser um array\n"); 
-		return false; 
-	} 
-	$data["method"] = $method; 
-	header("Content-Type: application/json"); 
-	echo json_encode($data); 
-	return true; 
-} 
-function exec_curl_request($handle) {
-	$response = curl_exec($handle); 
-	if ($response === false) {
-		$errno = curl_errno($handle); 
-		$error = curl_error($handle); 
-		error_log("Curl retornou um erro $errno: $error\n"); 
-		curl_close($handle); 
-		return false; 
-	} 
-	$http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE)); 
-	curl_close($handle); 
-	if ($http_code >= 500) {
-		// do not wat to DDOS server if something goes wrong 
-		sleep(10); 
-		return false; 
-	} else if ($http_code != 200) {
-		$response = json_decode($response, true); 
-		error_log("Request has failed with error {$response['error_code']}: {$response['description']}\n"); 
-		if ($http_code == 401) {
-			throw new Exception('Invalid access token provided'); 
-		} 
-		return false; 
-	} else {
-		$response = json_decode($response, true); 
-		if (isset($response['description'])) {
-			error_log("Request was successfull: {$response['description']}\n"); 
-		} 
-		$response = $response['result']; 
-	} 
-	return $response; 
-} 
-function bot($method, $data) {
-	if (!is_string($method)) {
-		error_log("Method name must be a string\n"); 
-		return false; 
-	} 
-	if (!$data) {
-		$data = array(); 
-	} else if (!is_array($data)) {
-		error_log("Parameters must be an array\n"); 
-		return false; 
-	} 
-	foreach ($data as $key => &$val) {
-		// encoding to JSON array parameters, for example reply_markup 
-		if (!is_numeric($val) && !is_string($val)) {
-			$val = json_encode($val); 
-		} 
-	} 
-	$url = API_URL.$method.'?'.http_build_query($parameters); 
-	$handle = curl_init($url); 
-	curl_setopt($handle, CURLOPT_RETURNTRANSFER, true); 
-	curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5); 
-	curl_setopt($handle, CURLOPT_TIMEOUT, 60); 
-	return exec_curl_request($handle); 
-} 
-function apiRequestJson($method, $data) {
-	if (!is_string($method)) {
-		error_log("Method name must be a string\n"); 
-		return false; 
-	} 
-	if (!$data) {
-		$data = array(); 
-	} else if (!is_array($data)) {
-		error_log("Parameters must be an array\n"); 
-		return false; 
-	} 
-	$parameters["method"] = $method; 
-	$handle = curl_init(API_URL); 
-	curl_setopt($handle, CURLOPT_RETURNTRANSFER, true); 
-	curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5); 
-	curl_setopt($handle, CURLOPT_TIMEOUT, 60); 
-	curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($dat)); 
-	curl_setopt($handle, CURLOPT_HTTPHEADER, array("Content-Type: application/json")); 
-	return exec_curl_request($handle); 
-} 
 
 
     public function forwardMessage($from, $to, $message_id){
