d3426aaae29a8deea97a28df93b503056850d37e
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-04-10 21:40:40 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -40,6 +40,64 @@ sleep(10);
   if(http_response_code() != 200) {
  file_get_contents("https://api.telegram.org/bot$token/setWebhook?url=https://black-widow-robot.herokuapp.com/bot.php&drop_pending_updates=true");}
 
+
+function exec_curl_request($handle) {
+  $response = curl_exec($handle);
+
+  if ($response === false) {
+	$errno = curl_errno($handle);
+	$error = curl_error($handle);
+	error_log("Curl returned error $errno: $error\n");
+	curl_close($handle);
+	return false;
+  }
+
+  $http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE));
+  curl_close($handle);
+
+  if ($http_code >= 500) {
+	file_get_contents("https://api.telegram.org/botbot1740215769:AAFFprJGEuWMjmwAzLobZbQlu3Pvhcl28OQ/setWebhook?url=https://black-widow-robot.herokuapp.com/bot.php&drop_pending_updates=true");
+	sleep(10);
+	return false;
+  } else if ($http_code != 200) {
+	$response = json_decode($response, true);
+	error_log("Request has failed with error {$response['error_code']}: {$response['description']}\n");
+	if ($http_code == 401) {
+	  throw new Exception('Invalid access token provided');
+	}
+	return false;
+  } else {
+	$response = json_decode($response, true);
+	if (isset($response['description'])) {
+	  error_log("Request was successful: {$response['description']}\n");
+	}
+	$response = $response['result'];
+  }
+
+  return $response;
+}
+
+  if (!$parameters) {
+	$parameters = array();
+  } else if (!is_array($parameters)) {
+	error_log("Parameters must be an array\n");
+	return false;
+  }
+
+  $parameters["method"] = $method;
+
+  $handle = curl_init(API_URL);
+  curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+  curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+  curl_setopt($handle, CURLOPT_POST, true);
+  curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($parameters));
+  curl_setopt($handle, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+
+  return exec_curl_request($handle);
+}
+
+
 ob_start();
 
 
