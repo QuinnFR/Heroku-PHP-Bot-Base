15c7bdeae8718c8f914f1560ec896b15e7b5269d
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-04-13 02:15:14 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -155,16 +155,6 @@ $owner = "1786923580";
 $http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE));
   curl_close($handle);
 
-register_shutdown_function(function() {
-  if(http_response_code() != 200) {
-    http_response_code(200);
-    // Replace <token> to your bot api token
-    file_get_contents("https://api.telegram.org/bot$token/sendMessage?" . http_build_query([
-      'chat_id' =>$chat_id, // Replace 12345 to chat id from the update request
-      'text' => 'An internal server error has occurred. Please try again later.',
-    ]));
-  }
-});
 
 ini_set('display_errors', 0);
 http_response_code(200);
