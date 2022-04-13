ed9a1764d38acdb45f639403fe1c9996bfffbd14
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-04-13 22:33:27 +0300

    Update Telegram.class2.php

--- class/Telegram.class2.php
+++ class/Telegram.class2.php
@@ -1,14 +1,14 @@
 <?php
 
 
-    public function forwardMessage($from, $to, $message_id){
+     function forwardMessage($from, $to, $message_id){
         return bot('forwardMessage' , [
             'chat_id'  => $to,
             'from_chat_id' =>$chat_id,
             'message_id' => $message_id,
         ]);
     }
-    public function copyMessage($from , $to , $message_id , $reply = null,$reply_to_message_id = null){
+     function copyMessage($from , $to , $message_id , $reply = null,$reply_to_message_id = null){
         return bot('copyMessage' , [
             'chat_id'  => $to,
             'from_chat_id' => $from,
@@ -18,14 +18,14 @@
         ]);
     }
 
-    public function answerCallbackQuery($alretcall , $text , $alert){
+     function answerCallbackQuery($alretcall , $text , $alert){
         return bot('answerCallbackQuery',[
             'callback_query_id' => $alretcall,
             'text' => $text,
             'show_alert' => $alert
         ]);
     }
-    public function edit_replay($chatid , $msgid ,$reply){
+     function edit_replay($chatid , $msgid ,$reply){
 	return $this->bot('editMessageReplyMarkup',[
             'chat_id'=>$chatid,
             'message_id'=>$msgid,
@@ -34,23 +34,23 @@
 		]);
 	}
 
-    public function Random_msg_top($alretcall, $random_alret){
+     function Random_msg_top($alretcall, $random_alret){
         return bot('answercallbackquery',[
             'callback_query_id' =>$alretcall,
             'text'=>$random_alret,]);}
 
 
-    Public function typing($chat_id, $action = 'typing'){ 
+     function typing($chat_id, $action = 'typing'){ 
         return bot("sendChatAction",[
             'chat_id'=>$chat_id,
             'action'=>$action]);}
 
-    public function jsonData($chat_id, $text){
+     function jsonData($chat_id, $text){
        return bot('sendMessage',[
             'chat_id' =>$chat_id,
             'text' =>$text]);}
 
-    public function sendMessage($chat_id, $text, $replyMarkup){
+     function sendMessage($chat_id, $text, $replyMarkup){
          return bot('sendMessage',[
             'chat_id'=>$chat_id,
             'text'=>$text,
@@ -58,12 +58,12 @@
             'parse_mode'=> 'HTML',
             'reply_markup'=>$replyMarkup]);}
       
-    public function sendMessageLite($chat_id, $text){
+     function sendMessageLite($chat_id, $text){
          return bot('sendMessage',[
             'chat_id'=>$chat_id,
             'text'=>$text]);}
 
-    public function sendMessageInlineKeyboard($chat_id, $text, $replyMarkup){
+     function sendMessageInlineKeyboard($chat_id, $text, $replyMarkup){
          return bot('sendMessage',[
             'chat_id'=>$chat_id,
             'text'=>$text,
@@ -71,7 +71,7 @@
             'parse_mode'=>'HTML',
             'reply_markup' =>$replyMarkup]);}
 
-    public function sendPhoto($chat_id, $photo, $caption = null){
+     function sendPhoto($chat_id, $photo, $caption = null){
           return bot('sendphoto',[
              'chat_id'=>$chat_id,
              'photo'=>$photo,
@@ -79,7 +79,7 @@
              'protect_content'=>true,
              'caption'=>$caption]);}
 
-    public function sendPhotoViaID($chat_id, $photo, $caption = null){
+     function sendPhotoViaID($chat_id, $photo, $caption = null){
            return bot('sendphoto',[
               'chat_id'=>$chat_id,
               'photo'=>$photo,
@@ -87,7 +87,7 @@
               'protect_content'=>true,
               'caption'=>$caption]);}
 
-    public function sendAudio($chat_id, $audio, $caption, $title ,$performer = null){
+     function sendAudio($chat_id, $audio, $caption, $title ,$performer = null){
             return bot('sendaudio',[
               'chat_id'=>$chat_id,
               'audio'=>$audio,
@@ -97,7 +97,7 @@
               'title'=>$title,
               'performer'=>$performer]);}
 
-    public function sendDocument($chat_id, $document, $caption, $replyMarkup){
+     function sendDocument($chat_id, $document, $caption, $replyMarkup){
             return bot('senddocument',[
               'chat_id'=>$chat_id,
               'document'=>$document,
@@ -106,7 +106,7 @@
               'parse_mode'=>'HTMl',
               'reply_markup' =>$replyMarkup]);}
 
-   public function sendVideo($chat_id, $video, $caption){
+    function sendVideo($chat_id, $video, $caption){
             return bot('sendvideo',[
               'chat_id'=>$chat_id,
               'video'=>$video,
@@ -114,7 +114,7 @@
               'protect_content'=>true,
               'caption'=>$caption]);}
 
-   public function sendVoice($chat_id, $voice, $caption){
+    function sendVoice($chat_id, $voice, $caption){
             return bot('sendvoice',[
               'chat_id'=>$chat_id,
               'voice'=>$voice,
@@ -122,7 +122,7 @@
               'protect_content'=>true,
               'caption'=>$caption]);}
 
-   public function alret($alretcall, $text, $showAlert = false){
+    function alret($alretcall, $text, $showAlert = false){
             return bot('answerCallbackQuery', [
               'callback_query_id' => $alretcall,
               'text' =>$text,
@@ -130,7 +130,7 @@
                                 ]);
                       }
 
-   public function Mute_New_Chat_Members($chat_id, $new_chat_member_id, $time){
+    function Mute_New_Chat_Members($chat_id, $new_chat_member_id, $time){
            return bot('restrictChatMember', [
               'chat_id' => $chat_id,
               'user_id' => $new_chat_member_id,
@@ -141,25 +141,25 @@
               'can_add_web_page_previews' => false
                                   ]);}
 
-   public function Delete($chat_id, $message_id){
+    function Delete($chat_id, $message_id){
           return bot('deleteMessage', [
               'chat_id'=> $chat_id,
               'message_id'=>$message_id ]);}
 
-   public function pin($chat_id, $message_id){
+    function pin($chat_id, $message_id){
          return bot('pinChatMessage', [
               'chat_id'=> $chat_id,
               'message_id'=>$message_id ]);}
 
-   public function unpin($chat_id){
+    function unpin($chat_id){
          return bot('unpinAllChatMessages', [
               'chat_id'=> $chat_id]);}
 
-   public function leaveChat($chat_id){
+    function leaveChat($chat_id){
          return bot('LeaveChat', [
               'chat_id'=> $chat_id ]);}
 
-   public function editMessageText($chat_id, $message_id, $text, $replyMarkup){
+    function editMessageText($chat_id, $message_id, $text, $replyMarkup){
        return bot('editMessageText', [
               'chat_id' => $chat_id,
               'message_id' => $message_id,
@@ -169,7 +169,7 @@
               'reply_markup' => $replyMarkup,
         ]);
     }
-   public function editMessageTextCallBack($chat_id, $message_id, $text, $parseMode = null, $disablePreview = false, $replyMarkup = null, $inlineMessageId = null){
+    function editMessageTextCallBack($chat_id, $message_id, $text, $parseMode = null, $disablePreview = false, $replyMarkup = null, $inlineMessageId = null){
        return bot('editMessageText', [
              'chat_id' => $chat_id,
              'message_id' => $message_id,
@@ -181,14 +181,14 @@
         ]);
     }
 
-   public function getChatMemberStatus($chat_id, $user_id){
+    function getChatMemberStatus($chat_id, $user_id){
       return  $getstatus = bot('getChatMember', [
             'chat_id'=> $chat_id,
             'user_id'=> $user_id]);
             $st = $getstatus->result->status;}
 
 
-   public function rss($line){
+    function rss($line){
       return $url = 'https://daryo.uz/feed/';
              $rss = simplexml_load_file($url);
       foreach ($rss->channel->item as $item){
@@ -196,7 +196,7 @@
               break;}}
 
 //in $media send an associative array with type and id/link/path. Exaple: $media = [['type' => 'photo', 'media' => '1234'], ['type' => 'video', 'media' => 'https://site/video.mp4']] https://core.telegram.org/bots/api#inputmedia
-   public function sendMediaGroup($chat_id, $media, $disable_notification = null, $reply_to_message_id = null){
+    function sendMediaGroup($chat_id, $media, $disable_notification = null, $reply_to_message_id = null){
              $media = json_encode($media);
              $args = [
                       'chat_id' => $chat_id,
@@ -206,7 +206,7 @@
 }
 
 
-   public function answerInlineQuery($inline_query_id, $results, $cache_time = 0, $is_personal = false, $next_offset = '', $switch_pm_text = '', $switch_pm_parameter = '') {
+    function answerInlineQuery($inline_query_id, $results, $cache_time = 0, $is_personal = false, $next_offset = '', $switch_pm_text = '', $switch_pm_parameter = '') {
         $res = bot('answerInlineQuery', [
             'inline_query_id' => $inline_query_id,
             'results' => json_encode($results),
@@ -219,7 +219,7 @@
         return $res;
     }
 
-   public function getChatMember($chat_id, $user_id){
+    function getChatMember($chat_id, $user_id){
         return bot('getChatMember', [
             'chat_id' => $chat_id,
             'user_id' => $user_id]); }
