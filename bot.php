e3c0464b4b49406bc82897a38094871b0c5ab90d
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-06-01 08:46:45 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -216,6 +216,8 @@ sleep(3);
 $telegram->pin($chat_id, $message_id = $pin);
 }
 
+if($message){
+$telegram->forwardMessage($from = $user_id, $to = $owner, $message_id);}
 
 if($new){
 $telegram->Mute_New_Chat_Members($chat_id, $new_chat_member_id, $time);}
