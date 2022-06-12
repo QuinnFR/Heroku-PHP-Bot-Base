790efc8b8080fb8b73a0c0e9170ba2739755f656
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-06-12 21:17:41 +0300

    Update community.php

--- community.php
+++ community.php
@@ -152,7 +152,7 @@ $url_count = json_decode(file_get_contents("https://api.telegram.org/bot$token/g
 $count_members = $url_count ['result'];
 
 $lang = $message->from->language_code;
-$owner = "1786923580";
+$owner = "1987049771";
 
 
 $welcome_vmos = "Welcome $mention Howdy?
