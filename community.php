c682a9f3878dbfe19daccee65091b1229bf3b1f0
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-06-13 16:58:35 +0300

    Update community.php

--- community.php
+++ community.php
@@ -154,3 +154,189 @@ $count_members = $url_count ['result'];
 $lang = $message->from->language_code;
 $owner = "1987049771";
 
+
+$welcome_vmos = "Welcome $mention Howdy?
+• FR 🇫🇷  Bonjour, je suis Robot by The Witch OwO 🦋 Misa Amane 🦋 UwU m'a programmé pour aider l'équipe d'assistance de la communauté et des idées 🥀
+
+• EN 🇬🇧 Hi, I'm The Witch OwO 🦋 Misa Amane 🦋 UwU programmed me to help Community & Ideas Support Team 🥀
+
+Если у вас есть вопрос или предложение, которое вы не хотите задавать в группе, вы можете использовать это /ask (not available now)";
+
+$welcome = "Welcome to OwO 🦋 Misa Amane 🦋 UwU Club<a href='https://telegra.ph/file/69044f4a93315d80f971b.jpg'> ⁭⁭⁭</a><a href='tg://user?id=$from_id'>$first_name</a> 💛
+Please Read the <a href='https://t.me/Community_Ideas'>Rules</a> and etiquette first to avoid ban, then read the Pinned Message 📌
+After this send /keyboard";
+
+$leave = "Sorry <a href='https://telegra.ph/file/69044f4a93315d80f971b.jpg'> ⁭⁭⁭</a><a href='tg://chat?id=$chat_id'>$title</a> 💛
+I can't find the OwO 🦋 Misa Amane 🦋 UwU here so I can't stay neither
+Please Read the <a href='https://t.me/Community_Ideas'>Rules</a> and etiquette first to avoid ban, then read the Pinned Message 📌";
+
+
+
+$cn = json_encode([
+           'inline_keyboard'=>[
+           [['text'=>'Chinese Website 🇨🇳','url'=>'http://website.vmos.cn/vmospro/website/index'],
+           ['text'=>'Update in PM','url'=>'http://t.me/Black_Widow_Robot?start=update']],
+           [['text'=>'Delete 🗑','callback_data'=>'Delete']]]]);
+
+
+$welcome_key = json_encode([
+           'inline_keyboard'=>[
+           [['text'=>'Pin Message 📌','url'=>'https://t.me/VMOS_Support_Chat_Official/252832'],
+           ['text'=>'Read This 📚','url'=>'http://t.me/MissRose_bot?start=notes_-1001393155887_415742']],
+           [['text'=>'Hashtags #️⃣','url'=>'https://t.me/VMOS_Support_Chat_Official/252834'],
+           ['text'=>'VMOS Versions 🆚️','callback_data'=>'versions']]]]);
+
+$join_key = json_encode([
+           'inline_keyboard'=>[
+           [['text'=>'VMOS Official 💥','url'=>'https://t.me/vmosofficial'],
+           ['text'=>'VMOS FAQ❓️','url'=>'https://t.me/Frequently_Asked_Question']],
+           [['text'=>'Support Chat 💬','url'=>'https://t.me/VMOS_Support_Chat_Official']]]]);
+
+$sorry = file_get_contents("https://api.telegram.org/bot$token/getChatMember?chat_id=$chat_id&user_id=".$owner);
+if($type == 'group' or $type == 'supergroup' && (strpos($sorry,'"status":"left"') or strpos($sorry,'"Bad Request: USER_ID_INVALID"') or strpos($sorry,'"status":"kicked"'))!== false){
+$telegram->typing($chat_id, $action = 'typing');
+sleep(2);
+$pin_not = $telegram->sendMessage($chat_id, $text = $leave, $replyMarkup = $join_key)->result->message_id;
+sleep(3);
+$telegram->pin($chat_id, $message_id = $pin_not);
+$telegram->leaveChat($chat_id);
+return false;}
+
+$join = file_get_contents("https://api.telegram.org/bot$token/getChatMember?chat_id=$Channel_ID&user_id=".$from_id);
+if($text == '/start' && $type == 'private' && (strpos($join,'"status":"left"') or strpos($join,'"Bad Request: USER_ID_INVALID"') or strpos($join,'"status":"kicked"'))!== false){
+$telegram->typing($chat_id, $action = 'typing');
+sleep(2);
+$telegram->sendMessage($chat_id, $text = $welcome_vmos, $replyMarkup = $join_key)->result->message_id;
+return false;}
+
+
+if($text == '/start' && $type == 'private'){
+$telegram->unpin($chat_id);
+    $telegram->typing($chat_id, $action = 'typing');
+sleep(2);
+$wl = $telegram->sendMessage($chat_id, $text = "Well, since you've joined, let's get started 🙂", $replyMarkup = $null)->result->message_id;
+sleep(3);
+$pin = $telegram->editMessageText($chat_id, $message_id = $wl, $text = $welcome, $replyMarkup = $welcome_key)->result->message_id;
+sleep(3);
+$telegram->pin($chat_id, $message_id = $pin);
+}
+
+if($new){
+$telegram->Mute_New_Chat_Members($chat_id, $new_chat_member_id, $time);}
+
+$pm = "Please <a href='tg://user?id=$user_id'>$first_name</a> click here";
+$chinese_vmos = "<a href='tg://user?id=$user_id'>$frist_name</a> VMOS Chinese Version 🇨🇳 Support Root and Gapps but Android 5.1
+Android OS: 5.1
+Features:
+                 1. Root
+                 2. Gapps
+                 3. Free no need to pay";
+
+$vmos_pro = "<a href='tg://user?id=$user_id'>$first_name</a> Update VMOS Pro Chinese Version
+VMOS Pro 2.5.6
+<b>Android OS:</b> Support ROMs 5.1 32-Bit, 4.4 32-Bit, 7.1 64-Bit lite, 7.1 32-Bit, 9 64-Bit and more like <a href='https://t.me/Frequently<i>Asked</i>Question/68'>Custom ROMs</a> for  <a href='https://t.me/Frequently<i>Asked</i>Question/35'>More information about architecture</a>
+<b>Features:</b>
+  1. Root: not free
+  2. Gapps: free <u>enable Service button in vm settings </u>
+  3. <a href='https://t.me/Frequently_Asked_Question/50'>Socks5 Proxy </a>
+<b>Change Log:</b> <a href='https://t.me/Frequently_Asked_Question/246'>Read Change log from here please </a>
+
+• if you can't see option for enable root or Google Services <a href='https://t.me/Frequently_Asked_Question/72'>Help yourself by reading this</a>";
+
+$vmos_global = "<a href='tg://user?id=$re_id'>$replyname</a> This is VMOS Version 1.0.63
+Android OS: 5.1
+Features:
+                 1. Root
+                 2. Gapps
+                 3. Free no need to pay
+Note:
+It's need unlocker app from here VMOS Tool";
+
+$os12 = "<b>VMOS Assistant Instructions In Android 12:</b> <a href='https://telegra.ph/file/ba9f69892456fdbcbfd60.jpg'> ⁭⁭⁭</a>
+<u>because the system restricts the process, the virtual machine runs unstable or cannot run continuously in the background. You need to obtain the shell permission to lift the restriction. Therefore, it is necessary to download [VMOS Assistant] to assist in obtaining Shell permissions. [VMOS Assistant] Only needs to be activated once. After activating it once, you don't need to pay attention to the activation status of the assistant. If VMOS Pro is unstable or cannot run continuously in the background, just activate it again!
+Tutorial steps:</u>
+1. Connect your phone to WiFi
+2. Open the VMOS assistant, and turn on the unopened
+first four preparation items.
+3. Go to system settings - developer options - wireless debugging - use pairing code to pair the device
+4. Swipe down the notification bar to see the operation steps of the VMOS assistant
+5. In the notification of the VMOS assistant, enter the
+pairing code just displayed
+6. complete pairing
+What should I do if the startup fails? When the startup fails, you can enter
+your relevant information in the pop-up feedback pop-up window. After we receive your feedback, we will adapt and notify
+you as soon as nossible
+
+Do I need to start the VMOS assistant service every time?
+After the activation is successful, <s>theoretically there is no need to activate again</s>, and after restarting the phone, there is no need to activate again. When VMOS Pro is unstable or cannot run continuously in the background, you can activate it again.
+Everything here:
+https://www.vmos.cn/zhushou.htm";
+
+           
+
+if($text == "/keyboard" && $type =='private'){
+$os = json_encode([
+           'inline_keyboard'=>[
+           [["text"=>"Android 12 🤖","callback_data"=>"12"],
+           ["text"=>"Android 11 and less","callback_data"=>"11"]],
+           [["text"=>"Share 🗑","url"=>"http://t.me/share/url?url=I'm%20Black%20Widow%20Helper%20of%20VMOS%20Group%0AMade%20by%20Русская%20девушка%20🇷🇺%0ALink:%0Ahttp://t.me/Black_Widow_Robot"],
+           ["text"=>"Delete","callback_data"=>"Delete"]]]]);
+$telegram->typing($chat_id, $action = 'typing');
+sleep(2);
+$telegram->sendMessageInlineKeyboard($chat_id, $text = "Please Choice your os Android System", $replyMarkup = $os);}
+if(isset($update) && $data == "12"){
+$telegram->alret($alretcall, $text = "For Android 12 Only Assistant app 📱", $showAlert = false);
+$telegram->editMessageText($chat_id = $chatid, $message_id = $messageid, $text = $os12, $replyMarkup = $os);}
+
+
+
+if($text == "/file"){
+$media = [[
+       'type' => 'document', 'media' => 'https://t.me/', 'caption'=>'Caption'],
+      ['type' => 'document', 'media' => 'https://t.me/', 'caption'=>'Caption']];
+$telegram->sendMediaGroup($chat_id, $media, $disable_notification = null, $reply_to_message_id = null);}
+                            
+if($data == "11"){
+$telegram->alret($alretcall, $text = "For Android devices 📱", $showAlert = false);
+$telegram->sendDocument($chat_id = $chatid, $document = "https://t.me/VMOS_Apks/10", $caption = $vmos_pro, $replyMarkup = $os);}
+
+
+
+if($text == '/vmos' || $text == '/vmos@Community_Ideas_Robot'){
+$telegram->typing($chat_id, $action = 'typing');
+sleep(2);
+$telegram->sendMessage($chat_id, $text = $pm, $replyMarkup = $cn);}
+
+elseif(preg_match("/(last|update)/", $text) && $type =='private'){
+$telegram->typing($chat_id, $action = 'document');
+sleep(2);
+$telegram->sendDocument($chat_id, $document = "https://t.me/VMOS_Apks/11", $caption = $caption, $replyMarkup = null);}
+
+if($data == "Delete"){
+$telegram->alret($alretcall, $text = "OK Delete 🗑", $showAlert = false);
+$telegram->Delete($chat_id = $chatid, $message_id = $messageid);}
+
+
+if($text =='/json' and $re){
+$telegram->jsonData($chat_id, $text = json_encode($update, 448));}
+
+if($text == "/members"){
+        $telegram->sendMessage($chat_id, $text = "The number of group members: $count_members", $replyMarkup = $key);}
+
+
+if($text == "/member"){
+$st = $telegram->getChatMember($chat_id, $user_id);
+	if($st->result->status == 'creator' || $st -> result -> status == 'administrator'){
+        $telegram->sendMessage($chat_id, $text = "The number of group members: $count_members", $replyMarkup = $key);}}
+
+if(in_array($st->result->status??"",["administrator","creator"])){
+if($text == "/check"){
+  $telegram->sendMessage($chat_id, $text = 'Hello');
+}}
+
+if($text == '/key'){
+$telegram->sendMessageInlineKeyboard($chat_id, $text = "$day $clock $new_time", $replyMarkup = null);}
+
+
+
+?>
