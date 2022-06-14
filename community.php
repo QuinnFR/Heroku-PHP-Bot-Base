050957d24d4739c3fa41acc3b2ecc51bd226e8bf
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-06-14 06:15:20 +0300

    Update community.php

--- community.php
+++ community.php
@@ -209,7 +209,7 @@ $telegram->Mute_New_Chat_Members($chat_id, $new_chat_member_id, $time);}
 
 $pm = "Please <a href='tg://user?id=$user_id'>$first_name</a> click here";
 
-if(isset($update) && $data == "Games"){
+if($data == "Games"){
 $Games = json_encode([
            'inline_keyboard'=>[
            [["text"=>"Actually","callback_data"=>"12"],
@@ -218,8 +218,7 @@ $Games = json_encode([
            ["text"=>"Delete","callback_data"=>"Delete"]]]]);
 
 $telegram->alret($alretcall, $text = "new version of public games 🎮", $showAlert = false);
-$telegram->sendMessage($chat_id, $text = "Well, since you've joined, let's get started 🙂", $replyMarkup = $Games);}
-
+$telegram->editMessageText($chat_id = $chatid, $message_id = $messageid, $text = "Please Choice your game", $replyMarkup = $Games);}
 
 if($text == "/file"){
 $media = [[
