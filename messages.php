738f0e39be64dc35544d1b5d2b410527e4347f18
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-06-14 14:22:50 +0300

    Update messages.php

--- messages.php
+++ messages.php
@@ -65,10 +65,7 @@ Features:
                  1. Root
                  2. Gapps
                  3. Free no need to pay";
-$whatsapp ="Whatsapp: 2.22.13.70-221370000
-Android 4.1: Arch (arm64-v8a,armeabi-v7a,x86,x86_64)(nodpi)
-<a href='https://www.apkmirror.com/apk/whatsapp-inc/whatsapp/'>Whatsapp APKMirror Update</a>";
-$new_time";
+
 
 
 ?>
