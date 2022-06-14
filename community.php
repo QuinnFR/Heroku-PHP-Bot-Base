070e23c771ead9bc545bd58214b9e48840ca4760
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-06-14 04:30:30 +0300

    Update community.php

--- community.php
+++ community.php
@@ -218,45 +218,6 @@ Features:
                  2. Gapps
                  3. Free no need to pay";
 
-$vmos_pro = "<a href='tg://user?id=$user_id'>$first_name</a> Update VMOS Pro Chinese Version
-VMOS Pro 2.5.6
-<b>Android OS:</b> Support ROMs 5.1 32-Bit, 4.4 32-Bit, 7.1 64-Bit lite, 7.1 32-Bit, 9 64-Bit and more like <a href='https://t.me/Frequently<i>Asked</i>Question/68'>Custom ROMs</a> for  <a href='https://t.me/Frequently<i>Asked</i>Question/35'>More information about architecture</a>
-<b>Features:</b>
-  1. Root: not free
-  2. Gapps: free <u>enable Service button in vm settings </u>
-  3. <a href='https://t.me/Frequently_Asked_Question/50'>Socks5 Proxy </a>
-<b>Change Log:</b> <a href='https://t.me/Frequently_Asked_Question/246'>Read Change log from here please </a>
-
-• if you can't see option for enable root or Google Services <a href='https://t.me/Frequently_Asked_Question/72'>Help yourself by reading this</a>";
-
-$vmos_global = "<a href='tg://user?id=$re_id'>$replyname</a> This is VMOS Version 1.0.63
-Android OS: 5.1
-Features:
-                 1. Root
-                 2. Gapps
-                 3. Free no need to pay
-Note:
-It's need unlocker app from here VMOS Tool";
-
-$os12 = "<b>VMOS Assistant Instructions In Android 12:</b> <a href='https://telegra.ph/file/ba9f69892456fdbcbfd60.jpg'> ⁭⁭⁭</a>
-<u>because the system restricts the process, the virtual machine runs unstable or cannot run continuously in the background. You need to obtain the shell permission to lift the restriction. Therefore, it is necessary to download [VMOS Assistant] to assist in obtaining Shell permissions. [VMOS Assistant] Only needs to be activated once. After activating it once, you don't need to pay attention to the activation status of the assistant. If VMOS Pro is unstable or cannot run continuously in the background, just activate it again!
-Tutorial steps:</u>
-1. Connect your phone to WiFi
-2. Open the VMOS assistant, and turn on the unopened
-first four preparation items.
-3. Go to system settings - developer options - wireless debugging - use pairing code to pair the device
-4. Swipe down the notification bar to see the operation steps of the VMOS assistant
-5. In the notification of the VMOS assistant, enter the
-pairing code just displayed
-6. complete pairing
-What should I do if the startup fails? When the startup fails, you can enter
-your relevant information in the pop-up feedback pop-up window. After we receive your feedback, we will adapt and notify
-you as soon as nossible
-
-Do I need to start the VMOS assistant service every time?
-After the activation is successful, <s>theoretically there is no need to activate again</s>, and after restarting the phone, there is no need to activate again. When VMOS Pro is unstable or cannot run continuously in the background, you can activate it again.
-Everything here:
-https://www.vmos.cn/zhushou.htm";
 
            
 
@@ -273,7 +234,6 @@ $telegram->sendMessageInlineKeyboard($chat_id, $text = "Please Choice your os An
 if(isset($update) && $data == "12"){
 $telegram->alret($alretcall, $text = "For Android 12 Only Assistant app 📱", $showAlert = false);
 $telegram->editMessageText($chat_id = $chatid, $message_id = $messageid, $text = $os12, $replyMarkup = $os);}
-
 
 
 if($text == "/file"){
