db46897169792475ef7ef9522c721c3f52994ad5
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-06-14 04:27:53 +0300

    Update community.php

--- community.php
+++ community.php
@@ -28,6 +28,7 @@ $update = json_decode($input);
 include 'class/Telegram.class.php';
 include 'iTelegram.php';
 include 'inline.php';
+include 'messages.php';
 
 $telegram = new Telegram($token);
 $message = $update->message;
@@ -154,21 +155,6 @@ $count_members = $url_count ['result'];
 $lang = $message->from->language_code;
 $owner = "1987049771";
 
-
-$welcome_vmos = "Welcome $mention Howdy?
-• FR 🇫🇷  Bonjour, je suis Robot by The Witch OwO 🦋 Misa Amane 🦋 UwU m'a programmé pour aider l'équipe d'assistance de la communauté et des idées 🥀
-
-• EN 🇬🇧 Hi, I'm The Witch OwO 🦋 Misa Amane 🦋 UwU programmed me to help Community & Ideas Support Team 🥀
-
-Если у вас есть вопрос или предложение, которое вы не хотите задавать в группе, вы можете использовать это /ask (not available now)";
-
-$welcome = "Welcome to OwO 🦋 Misa Amane 🦋 UwU Club<a href='https://telegra.ph/file/69044f4a93315d80f971b.jpg'> ⁭⁭⁭</a><a href='tg://user?id=$from_id'>$first_name</a> 💛
-Please Read the <a href='https://t.me/Community_Ideas'>Rules</a> and etiquette first to avoid ban, then read the Pinned Message 📌
-After this send /keyboard";
-
-$leave = "Sorry <a href='https://telegra.ph/file/69044f4a93315d80f971b.jpg'> ⁭⁭⁭</a><a href='tg://chat?id=$chat_id'>$title</a> 💛
-I can't find the OwO 🦋 Misa Amane 🦋 UwU here so I can't stay neither
-Please Read the <a href='https://t.me/Community_Ideas'>Rules</a> and etiquette first to avoid ban, then read the Pinned Message 📌";
 
 
 
