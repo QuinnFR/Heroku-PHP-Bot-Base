4d448ae2330c34b66a18b1f6fea112d1b3e7fd1f
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-06-14 23:02:12 +0300

    Update community.php

--- community.php
+++ community.php
@@ -373,21 +373,37 @@ $telegram->sendMessageInlineKeyboard($chat_id, $text = "$day $clock $new_time", 
 if(mb_stripos($text,"/search") !== false){ 
 $matn = str_replace("/search ","",$text);
 $telegram->('sendMessage',[
-'chat_id'=>$chat_id,
-'text'=>"🔍 Searching
+'chat_id'=>$cid,
+'text'=>"🔍 Qidiruv 
 ☑️ : $matn",
 'parse_mode'=>"Markdown",
 'reply_markup'=> json_encode([
 'inline_keyboard'=>[
-[['text'=>"App store 🌐", 'url'=>"https://www.apple.com/us/search?q=$matn"],],
-[['text'=>"Google 📈", 'url'=>"https://www.google.com.iq/search?q=$matn"],
-],[
-['text'=>"Youtube 🎥", 'url'=>"https://m.youtube.com/results?q=$matn&sm=3"],],[
+[
+['text'=>"App store 🌐", 'url'=>"https://www.apple.com/us/search?q=$matn"],
+],
+[
+['text'=>"Google 📈", 'url'=>"https://www.google.com.iq/search?q=$matn"],
+],
+[
+['text'=>"Youtube 🎥", 'url'=>"https://m.youtube.com/results?q=$matn&sm=3"],
+],
+[
 ['text'=>"instagram 📯", 'url'=>"https://www.instagram.com/$matn"],
-],[
+],
+
+[
 ['text'=>"Telegram 📪", 'url'=>"https://www.telegram.me/$matn"],
-],[['text'=>"Github 🐱", 'url'=>"https://github.com/search?utf8=✓&q=$matn"],],]])]);
+],
+[
+['text'=>"Github 🐱", 'url'=>"https://github.com/search?utf8=✓&q=$matn"],
+],
+]
+])
+]);
+
 }
+
 
 
 ?>
