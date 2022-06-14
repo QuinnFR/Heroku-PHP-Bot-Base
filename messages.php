2fe41d27671a3676dc43ee221fd06e3fd09675b0
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-06-14 11:13:14 +0300

    Update messages.php

--- messages.php
+++ messages.php
@@ -9,13 +9,12 @@ $welcome_vmos = "Welcome $mention Howdy?
 • FR 🇫🇷  Bonjour, je suis Robot by The Witch OwO 🦋 Misa Amane 🦋 UwU m'a programmé pour aider l'équipe d'assistance de la communauté et des idées 🥀
 
 • EN 🇬🇧 Hi, I'm The Witch OwO 🦋 Misa Amane 🦋 UwU programmed me to help Community & Ideas Support Team 🥀
-
-Если у вас есть вопрос или предложение, которое вы не хотите задавать в группе, вы можете использовать это /ask (not available now)";
+";
 
 $welcome = "Welcome to OwO 🦋 Misa Amane 🦋 UwU Club<a href='https://telegra.ph/file/69044f4a93315d80f971b.jpg'> ⁭⁭⁭</a><a href='tg://user?id=$from_id'>$first_name</a> 💛
 Please Read the <a href='https://t.me/Community_Ideas'>Rules</a> and etiquette first to avoid ban, then read the Pinned Message 📌";
 
-$leave = "Sorry <a href='https://telegra.ph/file/69044f4a93315d80f971b.jpg'> ⁭⁭⁭</a><a href='tg://chat?id=$chat_id'>$title</a> 💛
+$leave = "Sorry <a href='https://telegra.ph/file/3e0e1d0b3c019fa78291b.jpg'> ⁭⁭⁭</a><a href='tg://chat?id=$chat_id'>$title</a> 💛
 I can't find the OwO 🦋 Misa Amane 🦋 UwU here so I can't stay neither
 Please Read the <a href='https://t.me/Community_Ideas'>Rules</a> and etiquette first to avoid ban, then read the Pinned Message 📌";
 
