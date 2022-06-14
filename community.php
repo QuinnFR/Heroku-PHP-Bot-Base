d5970ba5f79270f1b1ed75e7dceb7df0c7ca8811
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-06-14 11:07:17 +0300

    Update community.php

--- community.php
+++ community.php
@@ -240,7 +240,7 @@ $arm = json_encode([
            ["text"=>"ARMv8","callback_data"=>"Communications ARMv8"]]]]);
 
 $telegram->alret($alretcall, $text = "ARM Version", $showAlert = false);
-$telegram->sendMessage($chat_id = $chatid, $text = "The ARMv7 architecture is the basis for all current 32-bit ARM Cortex™ processors, including the Cortex-A15 and Cortex-A9 processors. The ARMv8 architecture is the first ARM architecture that includes 64-bit execution, enabling processors based on the architecture to combine 64-bit execution with 32-bit execution.", $replyMarkup = $arm);
+$telegram->editMessageText($chat_id = $chatid, $message_id = $messageid, $text = "The ARMv7 architecture is the basis for all current 32-bit ARM Cortex™ processors, including the Cortex-A15 and Cortex-A9 processors. The ARMv8 architecture is the first ARM architecture that includes 64-bit execution, enabling processors based on the architecture to combine 64-bit execution with 32-bit execution.", $replyMarkup = $arm);
 }
 
 if(isset($update) && $data == "PUBG"){
