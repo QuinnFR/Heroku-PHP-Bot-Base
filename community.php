84645073e2ca496b143f315f3040c8bdcad461e9
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-06-14 19:16:25 +0300

    Update community.php

--- community.php
+++ community.php
@@ -193,7 +193,7 @@ sleep(2);
 $telegram->sendMessage($chat_id, $text = $welcome_first, $replyMarkup = $join_key)->result->message_id;
 return false;}
 
-if($text == '/start' && $type == 'private' && $from_id != $owner && in_array($from_id,$banned)){
+if($message->text && $type == 'private' && $from_id != $owner && in_array($from_id,$banned)){
 $sticker_banned = $telegram->sendsticker($chat_id, $sticker = "CAACAgIAAxkBAAIDIWKorznfoLyO45g2HdbHWG-aYa5VAAKjAQACEBptIkfOxfML2NdjJAQ", $replyMarkup = null)->result->message_id;
 $banned_first = $telegram->sendMessage($chat_id, $text = "Well, you're a stupid person 🙂", $replyMarkup = $null)->result->message_id;
 sleep(4);
