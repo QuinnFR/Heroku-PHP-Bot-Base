224e89a521499e742c7db9e62f6c0d3ff94aa59c
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-06-19 00:12:06 +0300

    Update community.php

--- community.php
+++ community.php
@@ -155,10 +155,12 @@ $owner = "1987049771";
 $bannedd = array("1622270145", "21279152", "575505287", "154021101", "2126768633", "5367656309", "5310401468"); 
 $banned = array("16222765545", "65662627"); 
 
-
 $caption_cn = $update->channel_post->caption;
 $id_cn = $update->channel_post->chat->id;
 $message_id_cn = $update->channel_post->message_id;
+
+if($update->channel_post){
+$telegram->edit_caption($chat_id = $id_cn, $caption = $caption_cn, $messag_id =$message_id_cn ,$reply = null);}
 
 
 $cn = json_encode([
