945ef1b9cf466970dff8c25da536f5f6249dd573
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-06-23 02:15:47 +0300

    Update community.php

--- community.php
+++ community.php
@@ -397,6 +397,6 @@ if($text == '/key'){
 $telegram->sendMessageInlineKeyboard($chat_id, $text = "$day $clock $new_time", $replyMarkup = null);}
 
 if(preg_match("/(TikTok|تيك توك|تيكتوك)/", $text) && $type =='private'){
-$telegram->typing($chat_id, $action = 'document');}
+$telegram->typing($chat_id, $action = 'document');
 $telegram->sendMessage($chat_id, $text = "Hello", $replyMarkup = null);}
 ?>
