3b20a13b20419472519e50955e4398efc59db5df
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-06-23 03:30:03 +0300

    Update community.php

--- community.php
+++ community.php
@@ -412,7 +412,7 @@ if(isset($update) && $data == "Info CPU"){
 $telegram->sendDocument($chat_id = $chatid, $document = "BQACAgQAAxkBAAIG-2KzqcMriS2YLbJ0nVKN0hCpQTPVAAI1CwACCj7hUZbMdzITxrxbKQQ", $caption = "To know if your phone support Armv8a or Armv7a you need to download this app and check the photos", $replyMarkup = null);
 $pic1 = $telegram->sendPhoto($chat_id = $chatid, $photo = "AgACAgQAAxkBAAIG_2Kzqr6A8ik7g2BJ5PWWvHEoO9M-AALfuzEbFmyhUUieXS2MM-VKAQADAgADeQADKQQ", $caption = "Click on <U>CPU</U>\nYou will see like this means your phone support Armv8a 64-bit", $replyMarkup = null)->result->message_id;
 $pic2 = $telegram->sendPhoto($chat_id = $chatid, $photo = "AgACAgQAAxkBAAIG_mKzqmSPhIBOXuiPzvncCdJjr5BGAALeuzEbFmyhUQAB9bP942HoIwEAAwIAA3kAAykE", $caption = "Click on <U>CPU</U>\nYou will see like this means your phone support Armv7a 32-bit", $replyMarkup = $cpu_info)->result->message_id;
-sleep(6);
+sleep(12);
 $telegram->Delete($chat_id, $message_id = $pic1);
 $telegram->Delete($chat_id, $message_id = $pic2);
 
