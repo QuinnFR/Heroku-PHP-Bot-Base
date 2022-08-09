fe2b803ef8c57f7a728f682546122b37cd88c359
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-08-09 18:19:44 +0300

    Update community.php

--- community.php
+++ community.php
@@ -417,8 +417,12 @@ $telegram->Delete($chat_id, $message_id = $pic1);
 $telegram->Delete($chat_id, $message_id = $pic2);
 }
 
-if(isset($update) && $data == "TikTok"){
-$telegram->alret($alretcall, $text = "By this button you can know information ℹ️ about your arch if support 64-bit or 32-bit", $showAlert = true);
+
+$media = [[
+       'type' => 'document', 'media' => 'BQACAgQAAxkBAAMqYvJ3tj0cUR__xZaEQ85uubdV3DwAAvYNAALc75lT26P7gVNzbLMpBA', 'caption'=>"TikTok ARMv7 25.6.3 + [TikTok Plugin 2.5.1](https://t.me/nephobot?start=GFgC2OYcgIoTCdtPsYDVSYTe184RFCMcJuXFdolYD3oL24MSlrCQJT-cEFSFBvRV)", 'parse_mode'=>'markdown'],
+      ['type' => 'document', 'media' => 'BQACAgQAAxkBAAIGcmKw6Fq82CV0HuJHPxhq3YMwfDXNAALWDgACkCOIUcuzMSpPNDkUKAQ', 'caption'=>"TikTok ARMv8 25.6.2 + [TikTok Plugin 2.5.1](https://t.me/nephobot?start=GFgC2OYcgIoTCdtPsYDVSYTe184RFCMcJuXFdolYD3oL24MSlrCQJT-cEFSFBvRV)", 'parse_mode'=>'markdown']];
+if($text == "/tiktok"){
+$telegram->sendMediaGroup($chat_id, $media, $disable_notification = null, $reply_to_message_id = $message_id);
 }
 
 
