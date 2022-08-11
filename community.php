e70490db9e3d0e7c340fd73f1ddb63a6bcadd243
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-08-11 21:58:27 +0300

    Update community.php

--- community.php
+++ community.php
@@ -425,7 +425,10 @@ $tiktok = [[
        'type' => 'document', 'media' => 'BQACAgQAAxkBAAMpYvJ3tndG9sxzvNmSQ35JXt_KOWkAAvUNAALc75lTvA9-LrSDcIspBA', 'caption'=>"TikTok ARMv7 25.6.2 + [TikTok Plugin 2.5.1](https://t.me/nephobot?start=GFgC2OYcgIoTCdtPsYDVSYTe184RFCMcJuXFdolYD3oL24MSlrCQJT-cEFSFBvRV)", 'parse_mode'=>'markdown'],
       ['type' => 'document', 'media' => 'BQACAgQAAxkBAAMqYvJ3tj0cUR__xZaEQ85uubdV3DwAAvYNAALc75lT26P7gVNzbLMpBA', 'caption'=>"TikTok ARMv8 25.6.2 + [TikTok Plugin 2.5.1](https://t.me/nephobot?start=GFgC2OYcgIoTCdtPsYDVSYTe184RFCMcJuXFdolYD3oL24MSlrCQJT-cEFSFBvRV)", 'parse_mode'=>'markdown']];
 if($text == "/t"){
-$telegram->sendMediaGroup($chat_id = $chat_id, $media = $tiktok, $disable_notification = null, $reply_to_message_id = $replyid);
+$ss = $telegram->sendMediaGroup($chat_id = $chat_id, $media = $tiktok, $disable_notification = null, $reply_to_message_id = $replyid);
+sleep(5);
+$telegram->Delete($chat_id, $message_id = $ss);
+
 }
 
 if($text == "/m"){
