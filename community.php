d100b49b0053afbbbff37894720419434fb2255b
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-08-12 20:49:20 +0300

    Update community.php

--- community.php
+++ community.php
@@ -457,4 +457,44 @@ else{
 $telegram->sendMessage($chat_id, $text = "Thanks", $replyMarkup = null);}
 }
 
+if($text_inline == 'Hi'){
+    $telegram->('answerInlineQuery', 
+        [
+            'inline_query_id' => $inline_query_id,
+            'cache_time'=>'150', 
+            'results' => json_encode(
+            [[
+                'type' => 'article',
+                'id' =>base64_encode(rand(5,555)),
+                'thumb_url'=>"https://telegra.ph/file/aeff14fd95fcb41429a36.jpg",
+                'title' => "Title",
+                'description'=>"How to download our app?",
+                'url'=> "https://www.vmod.com",
+                'input_message_content' => ['parse_mode' => 'HTML', 'message_text' => "hello"],
+                'reply_markup' => [
+                'inline_keyboard' => [
+                        [
+                            ['text' => "ok", 'switch_inline_query' => "switch"],['text' => "ok", 'switch_inline_query' => "switch"]
+                        ]]]
+            ],[
+                'type' => 'article',
+                'id' =>base64_encode(rand(5,555)),
+                'thumb_url'=>"https://telegra.ph/file/aeff14fd95fcb41429a36.jpg",
+                'title' => "Explain",
+                'description'=>"PGT+ app",
+                'url'=> "https://www.google.com",
+                'input_message_content' => ['parse_mode' => 'HTML', 'message_text' => "hello"],
+                'reply_markup' => [
+                'inline_keyboard' => [
+                        [
+                            ['text' => "ok", 'switch_inline_query_current_chat' => "switch"],['text' => "ok", 'switch_inline_query_current_chat' => "switch"]
+                        ]]]
+            ]]
+            )
+        ]
+    );
+}
+
+
+
 ?>
